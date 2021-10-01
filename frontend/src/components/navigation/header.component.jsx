36a8c3a985ed9d306da3b029f5e3d33a25a5e41a
MJ Felix <mjfelixdev@gmail.com>
2021-10-01 14:44:16 +1300

    Frontend: add draft drawer

--- frontend/src/components/navigation/header.component.jsx
+++ frontend/src/components/navigation/header.component.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 
 import AuthenticationButton from './authentication-button.component';
 
-import { Container, AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
+import { Container, AppBar, Box, Toolbar, Typography, IconButton, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import BookIcon from '@mui/icons-material/Book';
+import AccountIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
     const { isLoading } = useAuth0();
+    const location = useLocation();
+
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const toggleDrawer = (isOpen) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setIsDrawerOpen(isOpen);
+    };
 
     return (
         <header>
@@ -24,6 +39,7 @@ const Header = () => {
                                         color="inherit"
                                         aria-label="menu"
                                         sx={{ mr: 2 }}
+                                        onClick={toggleDrawer(true)}
                                     >
                                         <MenuIcon />
                                     </IconButton>
@@ -36,12 +52,58 @@ const Header = () => {
                         </AppBar>
                     </Box>
 
-                    <NavLink to="/books/reading">Reading</NavLink>{' '}
-                    <NavLink to="/books/added">Not Started</NavLink>{' '}
-                    <NavLink to="/books/finished">Finished</NavLink>{' '}
-                    <NavLink to="/user">User</NavLink>{' '}
+                    <Drawer
+                        anchor={'left'}
+                        open={isDrawerOpen}
+                        onClose={toggleDrawer(false)}
+                    >
+                        <Box
+                            sx={{ width: 250 }}
+                            role="presentation"
+                            onClick={toggleDrawer(false)}
+                            onKeyDown={toggleDrawer(false)}
+                        >
+                            <List>
+                                <ListItem>
+                                    <ListItemText
+                                        primary={'Books'}
+                                        primaryTypographyProps={{
+                                            fontSize: 18,
+                                            fontWeight: 'medium'
+                                        }}
+                                    />
+                                </ListItem>
+                                <ListItem button component={Link} to='/books/reading'>
+                                    <ListItemIcon>
+                                        <MenuBookIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Reading'} />
+                                </ListItem>
+                                <ListItem button component={Link} to='/books/added'>
+                                    <ListItemIcon>
+                                        <LibraryBooksIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Not Started'} />
+                                </ListItem>
+                                <ListItem button component={Link} to='/books/finished'>
+                                    <ListItemIcon>
+                                        <BookIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Finished'} />
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem button component={Link} to='/user'>
+                                    <ListItemIcon>
+                                        <AccountIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'My Account'} />
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Drawer>
                 </>
-
             }
         </header>
     );
