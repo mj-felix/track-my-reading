ace94df93139973f92cd75a3655a95656c899711
MJ Felix <mjfelixdev@gmail.com>
2022-07-09 08:41:12 +1200

    Redirect to user page post login

--- frontend/src/components/routing/public-route.container.jsx
+++ frontend/src/components/routing/public-route.container.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const PublicRoute = ({ component: Component, ...rest }) => {
-    const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-    return (
-        <Route
-            {...rest}
-            render={
-                (props) => {
-                    return isLoading ? 'Loading ...' :
-
-                        isAuthenticated ? (
-                            <Redirect to='/books/reading' />
-                        ) : (
-                            <Component {...props} />
-                        );
-
-                }
-            }
-        />
-    );
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return isLoading ? (
+          "Loading ..."
+        ) : isAuthenticated ? (
+          <Redirect to="/user" />
+        ) : (
+          <Component {...props} />
+        );
+      }}
+    />
+  );
 };
 
 export default PublicRoute;
