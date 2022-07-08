144002de0f1b110f0193f3232c81b79c4f0d13fe
MJ Felix <mjfelixdev@gmail.com>
2022-07-09 08:39:42 +1200

    Print auth token to console

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from 'react';
-import { Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './App.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
-import PrivateRoute from './components/routing/private-route.container';
-import PublicRoute from './components/routing/public-route.container';
+import PrivateRoute from "./components/routing/private-route.container";
+import PublicRoute from "./components/routing/public-route.container";
 
-import HomePage from './pages/public/home.page';
-import UserPage from './pages/private/user.page';
-import BookPage from './pages/private/book.page';
-import BooksReadingPage from './pages/private/books-reading.page';
-import BooksAddedPage from './pages/private/books-added.page';
-import BooksFinishedPage from './pages/private/books-finished.page';
+import HomePage from "./pages/public/home.page";
+import UserPage from "./pages/private/user.page";
+import BookPage from "./pages/private/book.page";
+import BooksReadingPage from "./pages/private/books-reading.page";
+import BooksAddedPage from "./pages/private/books-added.page";
+import BooksFinishedPage from "./pages/private/books-finished.page";
 
-import Header from './components/navigation/header.component';
+import Header from "./components/navigation/header.component";
 
-import { storeToken, upsertUser } from './redux/actions/auth.actions';
-
+import { storeToken, upsertUser } from "./redux/actions/auth.actions";
 
 function App() {
   const dispatch = useDispatch();
-  const performLogout = useSelector(state => state.auth.performLogout);
+  const performLogout = useSelector((state) => state.auth.performLogout);
   const { isAuthenticated, user, getAccessTokenSilently, logout } = useAuth0();
 
   const storeUser = async () => {
     const token = await getAccessTokenSilently();
+    console.log(token);
     dispatch(storeToken(token));
     await dispatch(upsertUser(user.email));
   };
@@ -54,15 +54,18 @@ function App() {
     <>
       <CssBaseline />
       <Header />
-      <Container maxWidth='md'>
+      <Container maxWidth="md">
         <main>
           <Switch>
-            <PrivateRoute path='/books/reading' component={BooksReadingPage} />
-            <PrivateRoute path='/books/added' component={BooksAddedPage} />
-            <PrivateRoute path='/books/finished' component={BooksFinishedPage} />
-            <PrivateRoute path='/books/:id' component={BookPage} />
-            <PrivateRoute path='/user' component={UserPage} />
-            <PublicRoute path='/' component={HomePage} />
+            <PrivateRoute path="/books/reading" component={BooksReadingPage} />
+            <PrivateRoute path="/books/added" component={BooksAddedPage} />
+            <PrivateRoute
+              path="/books/finished"
+              component={BooksFinishedPage}
+            />
+            <PrivateRoute path="/books/:id" component={BookPage} />
+            <PrivateRoute path="/user" component={UserPage} />
+            <PublicRoute path="/" component={HomePage} />
           </Switch>
         </main>
       </Container>
